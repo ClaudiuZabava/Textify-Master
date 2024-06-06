e3ce89abd92f498209abfe3a2f35ec288bbddee9
ClaudiuZabava <79301248+ClaudiuZabava@users.noreply.github.com>
2024-06-06 21:48:05 +0300

    AuthScreen Interface

--- Textify/app/src/main/java/com/example/textify/activity/MainActivity.kt
+++ Textify/app/src/main/java/com/example/textify/activity/MainActivity.kt
@@ -1,28 +1,41 @@
 package com.example.textify.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.textify.R
-import com.example.textify.utils.Delayer
+import com.example.textify.utils.Constants
+import com.example.textify.utils.PreferenceHandler
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel by viewModels<Delayer>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         installSplashScreen().apply {
-            setKeepOnScreenCondition {
-            !viewModel.isReady.value
-          }
-
+            setKeepOnScreenCondition{ true }
+//            lifecycleScope.launch { delay(1000L)
+//                setKeepOnScreenCondition{ false }
+//            }
+            Handler(Looper.getMainLooper()).postDelayed({
+                var intent = Intent(this@MainActivity,AuthActivity::class.java)
+//                if(preferenceHandler.getBoolean(Constants.KEY_IS_SIGNED_IN)) {
+//                    intent = Intent(this@SplashActivity, MainActivity::class.java)
+//                    ---> removed and putted outside      startActivity(intent)
+//                  finish()
+//               }
+                setKeepOnScreenCondition{ false }
+                startActivity(intent)
+           }, 1000)
         }
+        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -31,4 +44,5 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
 }
